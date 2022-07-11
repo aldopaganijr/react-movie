fb0c2bbc590f5c53b697be029cea18eb279f6651
aldopaganijr <aldojpagani@gmail.com>
2022-07-11 15:31:06 -0400

    some more files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import Header from "./components/Header";
+import "./styles/app.scss";
 
 function App() {
   return (
 
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
