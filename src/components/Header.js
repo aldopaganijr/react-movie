306421ae7c209878b6ca03b6df4b29613f591ddd
aldopaganijr <aldojpagani@gmail.com>
2022-07-13 16:21:22 -0400

    search page done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import blockbuster from "../images/blockbuster-logo.png"
 
 function Header() {
   return (
-
+        <>
           <span onClick={() => window.scroll(0,0)} className='header'>
-              BLOCKBUSTER
+              <p className='header-name'>BLOCKBUSTER</p>
+              <a href="https://bendblockbuster.com/" alt="original-store">
+              <img className="sign" src={blockbuster} alt="sign"/>
+              </a>
           </span>
           
-
+        </>
   )
 }
 
