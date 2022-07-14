be9de312d927f27c2b00bd7bcf095ce297954c32
aldopaganijr <aldojpagani@gmail.com>
2022-07-14 16:29:46 -0400

    page transition done

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import CustomPagination from '../../components/CustomPagination';
 import SingleContent from '../../components/SingleContent';
+import { motion } from "framer-motion";
+import { slider, sliderContainer } from '../../animation';
+import styled from "styled-components";
 
 
 const Movies = () => {
@@ -25,7 +28,14 @@ const Movies = () => {
 
   return (
     <>
-      <div className='pageTitle'>Movies</div>
+      <motion.div className='pageTitle' exit="exit"  initial="hidden" animate="show">Movies
+        <motion.div variants={sliderContainer}>
+          <Frame1 variants={slider}></Frame1>
+          <Frame2 variants={slider}></Frame2>
+          <Frame3 variants={slider}></Frame3>
+          <Frame4 variants={slider}></Frame4>
+        </motion.div>
+      </motion.div>
         <div className='trending'>
             {content && content.map((c) => 
             <SingleContent 
@@ -43,5 +53,26 @@ const Movies = () => {
     </>
   )
 }
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(to bottom, #02386E, #00172D);
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #FCC039;
+`;
+const Frame3 = styled(Frame1)`
+  background: linear-gradient(to bottom, #02386E, #00172D);
+`;
+const Frame4 = styled(Frame1)`
+  background: #FCC039;
+`;
+
+
 
 export default Movies
--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import SingleContent from '../../components/SingleContent';
 import CustomPagination from '../../components/CustomPagination';
+import { motion } from "framer-motion";
+import { slider, sliderContainer } from '../../animation';
+import styled from "styled-components";
 
 
 const Trending = () => {
@@ -28,7 +31,14 @@ const Trending = () => {
   return (
 
     <div>
-        <span className="pageTitle">Trending</span>
+        <motion.div className='pageTitle'  exit="exit"  initial="hidden" animate="show">Trending
+        <motion.div variants={sliderContainer}>
+          <Frame1 variants={slider}></Frame1>
+          <Frame2 variants={slider}></Frame2>
+          <Frame3 variants={slider}></Frame3>
+          <Frame4 variants={slider}></Frame4>
+        </motion.div>
+      </motion.div>
         <div className='trending'>
             {content && content.map((c) => 
             <SingleContent 
@@ -46,6 +56,25 @@ const Trending = () => {
     </div>
   )
 }
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(to bottom, #02386E, #00172D);
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #FCC039;
+`;
+const Frame3 = styled(Frame1)`
+  background: linear-gradient(to bottom, #02386E, #00172D);
+`;
+const Frame4 = styled(Frame1)`
+  background: #FCC039;
+`;
 
 export default Trending
 
